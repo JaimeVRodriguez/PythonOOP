76cfa59984e331504a2147ecd0e9f3a7ecbc5e48
JaimeVRodriguez <57820323+JaimeVRodriguez@users.noreply.github.com>
2021-02-11 10:11:18 -0500

    Add own details/examples

--- Basic/oop.py
+++ Basic/oop.py
@@ -1,10 +1,30 @@
 class Dog:
 
-    def __init__(self, name):
+    def __init__(self, name, human_age):
         self.name = name  # Attribute of dog
+        self.human_age = human_age
 
     def get_name(self):
         return self.name
+
+    def dog_age(self):
+        if self.human_age <= 1:
+            return 15
+        elif self.human_age <= 2:
+            return 24
+        else:
+            return 24 + ((self.human_age - 2) * 4)
+
+    def details(self):
+        name_message = f'Name: {self.name}'
+        h_age_message = f'Age (human years): {self.human_age}'
+        d_age_message = f'Age (dog years): {self.dog_age()}'
+
+        return f'''
+        {name_message}
+        {h_age_message}
+        {d_age_message}
+        '''
 
     def bark(self):
         print(f'{self.name} goes woof')
@@ -13,11 +33,8 @@ class Dog:
 # Instanciating
 # Variable d
 # Instance of dog class
-d = Dog('Chloe')
-print(d.get_name())
+d = Dog('Chloe', 6)
+print(d.details())
 
-d2 = Dog('Smalls')
-print(d2.get_name())
-
-
-d.bark()
+d2 = Dog('Smalls', 4)
+print(d2.details())
